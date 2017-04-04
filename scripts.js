7e0803cea7530d1cd44ca34f86dd04f76093284d
Micah Nisely <mjn@Waffle-Maker.MissouriState.edu>
2017-04-04 16:47:46 -0500

    no message

--- scripts.js
+++ scripts.js
@@ -30,5 +30,17 @@ function validateContactForm() {
 
 window.onload = initDate;
 function initDate() {
+  var today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth()+1;
+  var yyyy = today.getFullYear();
 
+  if(dd < 10) {
+    dd = '0' +dd
+  }
+  if(mm < 10) {
+    mm = '0' + mm
+  }
+  today = mm + '/' +dd+ '/' +yyyy;
+  document.getElementById('today_date').innerHTML = today;
 }
