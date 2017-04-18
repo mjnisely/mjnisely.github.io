b02fcb48b089ac6992194ab798295df2e533b204
Micah Nisely <mjn@Micahs-MacBook-Pro.local>
2017-04-18 09:41:42 -0500

    no message

--- scripts.js
+++ scripts.js
@@ -43,3 +43,13 @@ function initDate() {
   today = mm+'/'+dd+'/'+yyyy;
   document.getElementById('today_date').innerHTML = today;
 }
+
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide, 2000);
+
+function nextSlide() {
+  slides[currentSlide].className = 'slide';
+  currentSlide = (currentSlide+1)%slides.length;
+  slides[currentSlide].className = 'slide showing';
+}
