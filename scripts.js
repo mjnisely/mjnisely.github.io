2e655ed5e006ccbe4a2415a07f2e99732aed4d03
Micah Nisely <mjn@Micahs-MacBook-Pro.local>
2017-04-20 11:38:04 -0500

    no message

--- scripts.js
+++ scripts.js
@@ -44,12 +44,17 @@ function initDate() {
   document.getElementById('today_date').innerHTML = today;
 }
 
-var slides = document.querySelectorAll('#slides .slide');
-var currentSlide = 0;
-var slideInterval = setInterval(nextSlide, 2000);
+var slideIndex = 0;
+showSlides();
 
-function nextSlide() {
-  slides[currentSlide].className = 'slide';
-  currentSlide = (currentSlide+1)%slides.length;
-  slides[currentSlide].className = 'slide showing';
+function showSlides() {
+  var i;
+  var slides = document.getElementByClassName("mySlides");
+  for(i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 2000); //Change ever 2 sseconds
 }
