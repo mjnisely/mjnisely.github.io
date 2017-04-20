fa791ca56526eb3ff7bb688e7ad8690cabe872af
Micah Nisely <mjn@Micahs-MacBook-Pro.local>
2017-04-20 11:57:49 -0500

    no message

--- scripts.js
+++ scripts.js
@@ -49,7 +49,7 @@ showSlides();
 
 function showSlides() {
   var i;
-  var slides = document.getElementByClassName("mySlides");
+  var slides = document.getElementsByClassName("mySlides");
   for(i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
